2cc20aa7f636886ad0e24c8fdcb4dcdeea7b90e6
MrPlax <aadam.blank@gmail.com>
2023-09-26 15:43:20 +0200

    fixed

--- Programowanie/ConsoleApplication1/ConsoleApplication1.cpp
+++ Programowanie/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,23 +2,23 @@
 #include <iostream>
 
 //1.Program obliczaj¹cy sredni¹ artmetyczn¹
-void task1() 
+void task1()
 {
 	double firstNumberFromUser, secondNumberFromUser;
-	std::cout << "podaj 1 liczbe\n";
+	std::cout << "Podaj 1 liczbe\n";
 	std::cin >> firstNumberFromUser;
-	std::cout << "podaj 2 liczbe\n";
+	std::cout << "Podaj 2 liczbe\n";
 	std::cin >> secondNumberFromUser;
 
-	std::cout << "œrednia artmetyczna tych 2 liczb wynosi: " << (firstNumberFromUser + secondNumberFromUser) / 2;
+	std::cout << "Œrednia artmetyczna tych 2 liczb wynosi: " << (firstNumberFromUser + secondNumberFromUser) / 2.0;
 }
 //2. Program obliczaj¹cy pole prostok¹ta.
-void task2() 
+void task2()
 {
 	double firstSideFromUser, secondSideFromUser;
-	std::cout << "podaj a\n";
+	std::cout << "Podaj a\n";
 	std::cin >> firstSideFromUser;
-	std::cout << "podaj b\n";
+	std::cout << "Podaj b\n";
 	std::cin >> secondSideFromUser;
 
 	std::cout << "Pole prostok¹ta wynosi " << (firstSideFromUser * secondSideFromUser);
@@ -27,18 +27,18 @@ void task2()
 void task3()
 {
 	double coneHeightFromUser, coneRadiusFromUser;
-	std::cout << "podaj wysokoœæ\n";
+	std::cout << "Podaj wysokoœæ\n";
 	std::cin >> coneHeightFromUser;
-	std::cout << "podaj promieñ\n";
+	std::cout << "Podaj promieñ\n";
 	std::cin >> coneRadiusFromUser;
 
-	std::cout << "Objêtoœæ sto¿ka wynosi " << (1.0/3.0 *M_PI) * pow(coneRadiusFromUser, 2) * coneHeightFromUser;
+	std::cout << "Objêtoœæ sto¿ka wynosi " << (1.0 / 3.0 * M_PI) * pow(coneRadiusFromUser, 2) * coneHeightFromUser;
 }
 //4. Program obliczaj¹cy pole ko³a.
 void task4()
 {
 	double circleRadiusFromUser;
-	std::cout << "podaj promieñ ko³a\n";
+	std::cout << "Podaj promieñ ko³a\n";
 	std::cin >> circleRadiusFromUser;
 
 	std::cout << "Pole ko³a wynosi " << M_PI * pow(circleRadiusFromUser, 2);
@@ -47,9 +47,9 @@ void task4()
 void task5()
 {
 	double firstNumberFromUser, secondNumberFromUser;
-	std::cout << "podaj 1 liczbe\n";
+	std::cout << "Podaj 1 liczbe\n";
 	std::cin >> firstNumberFromUser;
-	std::cout << "podaj 2 liczbe\n";
+	std::cout << "Podaj 2 liczbe\n";
 	std::cin >> secondNumberFromUser;
 
 	std::cout << "a^2+b^2 wynosi " << pow(firstNumberFromUser, 2) + pow(secondNumberFromUser, 2);
@@ -58,63 +58,70 @@ void task5()
 void task6()
 {
 	double triangleBaseFromUser, triangleHeightFromUser;
-	std::cout << "podaj podstawe trójk¹ta\n";
+	std::cout << "Podaj podstawe trójk¹ta\n";
 	std::cin >> triangleBaseFromUser;
-	std::cout << "podaj wysokoœæ trójk¹ta\n";
+	std::cout << "Podaj wysokoœæ trójk¹ta\n";
 	std::cin >> triangleHeightFromUser;
 
-	std::cout << "a^2+b^2 wynosi " << (triangleBaseFromUser, triangleHeightFromUser)/2.0;
+	std::cout << "Pole trójk¹ta wynosi " << (triangleBaseFromUser * triangleHeightFromUser) / 2.0;
 }
 //7. Program obliczaj¹cy objêtoœæ kuli o promieniu r
 void task7()
 {
 	double sphereRadiusFromUser;
-	std::cout << "podaj promieñ kuli\n";
+	std::cout << "Podaj promieñ kuli\n";
 	std::cin >> sphereRadiusFromUser;
 
-	std::cout << "objêtoœæ kuli wynosi " << (4/3.0 * M_PI) * pow(sphereRadiusFromUser, 3);
+	std::cout << "Objêtoœæ kuli wynosi " << (4.0 / 3.0 * M_PI) * (pow(sphereRadiusFromUser, 3));
 }
 //8. Program obliczaj¹cy pole trapezu o podstawach a i b oraz wysokoœci h
 void task8()
 {
-	double firstTrapezeBaseFromUser, secondTrapezeBaseFromUser , trapezeHeightFromUser;
-	std::cout << "podaj 1 podstawe trapezu\n";
+	double firstTrapezeBaseFromUser, secondTrapezeBaseFromUser, trapezeHeightFromUser;
+	std::cout << "Podaj 1 podstawe trapezu\n";
 	std::cin >> firstTrapezeBaseFromUser;
-	std::cout << "podaj 2 podstawe trapezu\n";
+	std::cout << "Podaj 2 podstawe trapezu\n";
 	std::cin >> secondTrapezeBaseFromUser;
-	std::cout << "podaj wysokoœæ trapezu\n";
+	std::cout << "Podaj wysokoœæ trapezu\n";
 	std::cin >> trapezeHeightFromUser;
 
 
-	std::cout << "Pole trapezu wynosi " << ((firstTrapezeBaseFromUser + secondTrapezeBaseFromUser) * trapezeHeightFromUser)/2;
+	std::cout << "Pole trapezu wynosi " << ((firstTrapezeBaseFromUser + secondTrapezeBaseFromUser) * trapezeHeightFromUser) / 2;
 }
 //9. Program obliczaj¹cy œredni¹ wa¿on¹ trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
 void task9()
 {
-	double firstNumberFromUser, secondNumberFromUser, thirdNumberFromUser , firstNumberWeightFromUser, secondNumberWeightFromUser, thirdNumberWeightFromUser;
-	std::cout << "podaj 1 liczbe\n";
+	double firstNumberFromUser, secondNumberFromUser, thirdNumberFromUser,
+		firstNumberWeightFromUser, secondNumberWeightFromUser, thirdNumberWeightFromUser;
+	std::cout << "Podaj 1 liczbe\n";
 	std::cin >> firstNumberFromUser;
-	std::cout << "podaj wage 1 liczby\n";
+	std::cout << "Podaj wage 1 liczby\n";
 	std::cin >> firstNumberWeightFromUser;
-	std::cout << "podaj 2 liczbe\n";
+	std::cout << "Podaj 2 liczbe\n";
 	std::cin >> secondNumberFromUser;
-	std::cout << "podaj wage 2 liczby\n";
+	std::cout << "Podaj wage 2 liczby\n";
 	std::cin >> secondNumberWeightFromUser;
-	std::cout << "podaj 3 liczbe\n";
+	std::cout << "Podaj 3 liczbe\n";
 	std::cin >> thirdNumberFromUser;
-	std::cout << "podaj wage 3 liczby\n";
+	std::cout << "Podaj wage 3 liczby\n";
 	std::cin >> thirdNumberWeightFromUser;
 
-	double FirstPartOfWeightedAverage = (firstNumberFromUser * firstNumberWeightFromUser) + (secondNumberFromUser * secondNumberWeightFromUser) + (thirdNumberFromUser * thirdNumberWeightFromUser);
-	double WeightedAverage = FirstPartOfWeightedAverage/((firstNumberWeightFromUser + secondNumberWeightFromUser + thirdNumberWeightFromUser));
-	std::cout << "œrednia wa¿ona tych liczb wynosi " << WeightedAverage;
+	double amountOfNumbers = (firstNumberFromUser * firstNumberWeightFromUser) +
+		(secondNumberFromUser * secondNumberWeightFromUser) +
+		(thirdNumberFromUser * thirdNumberWeightFromUser);
+	double amountOfWeight = ((firstNumberWeightFromUser + secondNumberWeightFromUser + thirdNumberWeightFromUser));
+
+	double WeightedAverage = amountOfNumbers / amountOfWeight ;
+
+	std::cout << "Œrednia wa¿ona tych liczb wynosi " << WeightedAverage;
 }
 int main()
 {
 	setlocale(LC_CTYPE, "Polish");
+
 	task9();
 
 
-}  
+}
 
 
