0ac55745529e305be4e30e7b13297182b39eeb1c
MrPlax <aadam.blank@gmail.com>
2023-10-10 15:53:55 +0200

    a

--- Programowanie/ConsoleApplication2/ConsoleApplication2.cpp
+++ Programowanie/ConsoleApplication2/ConsoleApplication2.cpp
@@ -96,24 +96,12 @@
  //6. Program sprawdzaj¹cy czy podana data jest poprawna (np. sprawdzaj¹c, czy dzieñ jest z zakresu od 1 do 31, miesi¹c od 1 do 12 itd.)
  bool task6_1(int day, int month, int year)
  {
-     if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 && day <= 31 && day > 0)
-         return true;
-     else if (month == 4 || month == 6 || month == 9 || month == 11 && day <= 30 && day > 0)
-         return true;
-     else if (month == 2)
-     {
-
-         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) //rok przestêpny
-         {
-             if (day > 0 && day <= 29)
-                 return true;
-         }
-         else if (day > 0 && day <= 28)
-             return true;
-         else
-             return false;
-     }
-     else
+     int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31 };
+     if (day < 1 || month > 12 || month < 1)
+         return false;
+     if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) //rok przestêpny
+         daysInMonth[1] = 29;
+     if (day > daysInMonth[month - 1])
          return false;
  }
  void task6()
@@ -145,6 +133,38 @@
      else
          std::cout << "optymalnie";
 
+ }
+
+ //8. Program sprawdzj¹cy czy podana liczba jest z przedia³u <1;15)
+ void task8()
+ {
+     double numberFromUser;
+     std::cin >> numberFromUser;
+     if(numberFromUser >= 1 || numberFromUser < 15)
+         std::cout << "liczba jest z przedia³u <1;15";
+     else
+         std::cout << "liczba nie jest z przedia³u <1;15";
+}
+ void task9() 
+ {
+     int AgeFromUser;
+
+     std::cin >> AgeFromUser;
+     if (AgeFromUser <18)
+         std::cout << "osoba pe³noletnia";
+     else
+         std::cout << "osoba nie pe³noletnia";
+
+ }
+ void task10() 
+ {
+     double sideFromUser1, sideFromUser2, sideFromUser3;
+
+     std::cin >> sideFromUser1;
+     std::cin >> sideFromUser2;
+     std::cin >> sideFromUser3;
+     if(sideFromUser1 + sideFromUser2 > sideFromUser3 && sideFromUser1 + sideFromUser3 > sideFromUser2 && sideFromUser2 + sideFromUser3 > sideFromUser1)
+         std::cout << "mo¿na zrobiæ trujk¹t";
  }
 int main()
 {
