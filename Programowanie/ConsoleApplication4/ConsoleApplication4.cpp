38b87ccc565a546a7882bb660c0ced6a12c41722
MrPlax <aadam.blank@gmail.com>
2023-11-17 13:08:30 +0100

    h

---
+++ Programowanie/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void task1()
+{
+	std::string charFromUser;
+	std::cout << "Podaj znak";
+
+	std::cout << "Twój znak to: " << charFromUser;
+}
+
+void task2()
+{
+	std::string userName;
+	std::cout << "jak masz na imie?";
+
+	std::cout << "Witaj" << userName;
+}
+int main()
+{
+
+
+
+	
+}
