c536ce27acc9b0d031287f0ec3d8457101be2dea
MrPlax <aadam.blank@gmail.com>
2023-11-21 16:46:07 +0100

    sd

--- Programowanie/ConsoleApplication4/ConsoleApplication4.cpp
+++ Programowanie/ConsoleApplication4/ConsoleApplication4.cpp
@@ -2,23 +2,32 @@
 
 void task1()
 {
-	std::string charFromUser;
-	std::cout << "Podaj znak";
-
+	char charFromUser;
+	std::cout << "Podaj znak \n";
+	std::cin >> charFromUser;
 	std::cout << "Twój znak to: " << charFromUser;
+	if (charFromUser >= 'a' && charFromUser <= 'z')
+		std::cout << "ma³a litera";
+	else
+		std::cout << "NIE ma³a litera";
+	charFromUser += 2;
+	std::cout << charFromUser;
+	
+	charFromUser = 'a' + 2;
+	std::cout << charFromUser;
+
 }
 
 void task2()
 {
-	std::string userName;
-	std::cout << "jak masz na imie?";
-
-	std::cout << "Witaj" << userName;
+	std::string nameFromUser;
+	std::cout << "jak masz na imie?\n";
+	std::cin >> nameFromUser;
+	std::cout << "Witaj" << nameFromUser;
 }
 int main()
 {
 
+	task2();
 
-
-	
 }
