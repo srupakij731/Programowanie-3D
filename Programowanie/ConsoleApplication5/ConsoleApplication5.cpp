1b4ded329bd1c20bbb7124984c01a65b6c61ee51
MrPlax <aadam.blank@gmail.com>
2023-12-18 11:21:06 +0100

    gt

--- Programowanie/ConsoleApplication5/ConsoleApplication5.cpp
+++ Programowanie/ConsoleApplication5/ConsoleApplication5.cpp
@@ -15,7 +15,36 @@ void task1()
 
 }
 
+void task2()
+{
+	const int UPPER_RANGE = 100;
+	const int LOWER_RANGE = 5;
+
+	const int SIZE_OF_ARRAY = 5;
+	int numbers[SIZE_OF_ARRAY];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++)
+	{
+		numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+	}
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++)
+		std::cout << numbers[i] << ", ";
+	std::cout << "\n";
+
+	int max = numbers[0], min = numbers[0];
+	for (int i = 1; i < SIZE_OF_ARRAY; i++)
+		if (numbers[i] > max)
+			max = numbers[i];
+
+	for (int i = 1; i < SIZE_OF_ARRAY; i++)
+		if (numbers[i] > min)
+			min = numbers[i];
+}
+
 int main()
 {
-	task1();
+	task2();
 }
