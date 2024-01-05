1ceac181ff661ecd8321939177badabb8613bc74
MrPlax <aadam.blank@gmail.com>
2024-01-05 13:09:38 +0100

    tak

--- Programowanie/ConsoleApplication5/ConsoleApplication5.cpp
+++ Programowanie/ConsoleApplication5/ConsoleApplication5.cpp
@@ -4,7 +4,7 @@ void task1()
 	float numbers[5];
 	for (int i = 0; i < 5; i++)
 	{
-		std::cout << "podaj " << i+1 << " liczbe \n ";
+		std::cout << "podaj " << i + 1 << " liczbe \n ";
 		std::cin >> numbers[i];
 	}
 
@@ -44,7 +44,64 @@ void task2()
 			min = numbers[i];
 }
 
+void task3()
+{
+	const int UPPER_RANGE = 10;
+	const int LOWER_RANGE = 1;
+
+	const int SIZE_OF_ARRAY = 2;
+	int numbers[SIZE_OF_ARRAY];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++)
+		numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++)
+		std::cout << numbers[i] << ", ";
+
+	std::cout << "\n";
+
+	double sum = 0;
+
+	for (int num : numbers)
+		sum += num;
+
+	std::cout << "œrednia to " << sum * 1.0 / SIZE_OF_ARRAY;
+}
+
+void task5()
+{
+	const int UPPER_RANGE = 3;
+	const int LOWER_RANGE = 1;
+
+	const int SIZE_OF_ARRAY = 10;
+	int numbers[SIZE_OF_ARRAY];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++)
+		numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++)
+		std::cout << numbers[i] << ", ";
+
+	std::cout << "\n";
+
+	for (int numberFromRange = LOWER_RANGE; numberFromRange <= UPPER_RANGE; numberFromRange++)
+	{
+		int count = 0;
+
+		for (int num : numbers)
+		{
+			if (num == numberFromRange)
+				count++;
+		}
+		std::cout << numberFromRange << " = " << count << "\n";
+	}
+}
+
 int main()
 {
-	task2();
+	task5();
 }
