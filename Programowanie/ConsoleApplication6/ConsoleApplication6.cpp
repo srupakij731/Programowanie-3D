3786cc4d9aede5ae8f592b15ccbf506923037b4f
MrPlax <aadam.blank@gmail.com>
2024-03-25 11:35:57 +0100

    a

--- Programowanie/ConsoleApplication6/ConsoleApplication6.cpp
+++ Programowanie/ConsoleApplication6/ConsoleApplication6.cpp
@@ -2,65 +2,68 @@
 #include <iostream>
 class C
 {
-private:
+protected:
     int fieldA;
+    std::string name;
 public:
+    C()
+    {
+        fieldA = 0;
+        name = "C";
+    }
+
+    C(int a, std::string x)
+    {
+        fieldA = a;
+        name = x;
+    }
+
     int getA()
     {
         return fieldA;
     }
-};
-class A
-{
-private:
-    //int fieldA;
-public:
-    A(int field)
-    {
-        fieldA = field;
-    }
+
     void ShowInfo()
     {
+        std::cout << "info dla klasy" << name;
         std::cout << fieldA;
+
+    }
+};
+class A : public C
+{
+public:
+    A(int field): C(1, "a")
+    {
+        name = "A";
+    }
+
+    A(int a, std::string x)
+    {
+        fieldA = a;
+        name = x;
     }
 
 };
-class B
+class B : public C
 {
 private:
-    //int fieldA;
     int fieldB;
 
 public:
     B(int field)
     {
-        fieldA = field;
+        fieldB = field;
     }
 
     int getB()
     {
         return fieldB;
     }
-    void ShowInfo()
-    {
-        std::cout << fieldA;
-        std::cout << fieldB;
 
-    }
 
 };
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
