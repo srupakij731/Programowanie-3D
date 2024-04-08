1b609d6de1ab6ba51d26c2e152952f9db7a28695
MrPlax <aadam.blank@gmail.com>
2024-04-08 11:38:55 +0200

    a

--- Programowanie/ConsoleApplication7/ConsoleApplication7.cpp
+++ Programowanie/ConsoleApplication7/ConsoleApplication7.cpp
@@ -4,27 +4,44 @@
 class Quadrangle
 {
 public:
+    Quadrangle(double sideA, double sideB, double sideC, double sideD, double height)
+    {
+        this->sideA = sideA;
+        this->sideB == sideB;
+        this->sideC == sideC;
+        this->sideD == sideD;
+        this->height == height;
+    }
+    double GetPerimeter()
+    {
+        return sideA + sideB + sideC + sideD;
+    }
 protected:
     double sideA, sideB, sideC, sideD, height;
 };
 
-class Square : public Quadrangle
-{
-public:
-    Square(double sideA)
-    {
-        this->sideA = height = sideD = sideC = sideB = sideA;
-    }
-};
+
 
 class Rectangle : public Quadrangle
 {
 public:
-    Rectangle(double sideA, double sideB)
+    Rectangle(double sideA, double sideB) : Quadrangle(sideA, sideB, sideA, sideB, sideB)
     {
-        this->sideA = sideC = sideA;
-        this->sideB = sideD = sideB;
     }
+    double GetArea()
+    {
+        return sideA * sideB;
+    }
+};
+
+class Square : public Rectangle
+{
+public:
+    Square(double side) : Rectangle(side, side)
+    {
+
+    }
+
 };
 
 class Trapeze : public Quadrangle
@@ -32,18 +49,20 @@ class Trapeze : public Quadrangle
 protected:
     
 public:
-    Trapeze(double sideA, double sideB, double sideC, double sideD, double height)
+    Trapeze(double sideA, double sideB, double sideC, double sideD, double height) 
+        : Quadrangle(sideA, sideB, sideC, sideD, height)
     {
-        this->sideA = sideA;
-        this->sideB ==sideB;
-        this->sideC == sideC;
-        this->sideD == sideD;
-        this->height == height;
+    }
+    double GetArea()
+    {
+        return ((sideA + sideC) * height)/2;
     }
 };
 int main()
 {
+    Quadrangle q(0, 0, 0, 0, 0);
     Square s(2);
+    s.GetPerimeter();
     Rectangle r(1,2);
     Trapeze t(1,2,3,4,5);
 
