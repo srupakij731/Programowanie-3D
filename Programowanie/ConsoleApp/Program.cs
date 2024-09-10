a6f38e874d02a0477c36c5918574d54a461af489
Uczen <Uczen@DESKTOP-6O2JQB3>
2024-09-10 15:49:48 +0200

    hej :D

---
+++ Programowanie/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("aaaa");
+string stringi = "E-Mail";
+Console.WriteLine("tekst" + stringi);
+Console.WriteLine("tekst {0}", stringi);
+Console.WriteLine($"tekst {stringi}");
+
+Console.WriteLine("km\\h");
+Console.WriteLine(@"km\h");
+
+int number = -69;
+Console.WriteLine(number);
+void function(int x)
+{
+    x += 1;
+}
+function(number);
+Console.WriteLine(number);
