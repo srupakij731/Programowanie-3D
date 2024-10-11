bcc756a6e9decf04b27954775c1e0d85054fc860
q <q>
2024-10-11 11:36:43 +0200

    sd

---
+++ Programowanie/CollectionConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿List<int> intsCollection = new List<int>();
+intsCollection.Add(15);
+intsCollection.Add(18);
+intsCollection.Add(5);
+intsCollection.Add(12);
+intsCollection.Add(19);
+intsCollection.Add(4);
+
+Console.WriteLine(intsCollection[0]);
+Console.WriteLine(intsCollection[1]);
+intsCollection.RemoveAt(1);
+Console.WriteLine(intsCollection[0]);
+Console.WriteLine(intsCollection[1]);
+
+Console.WriteLine("elementy w kolekcji");
+for (int i = 0; i < intsCollection.Count; i++)
+{
+    Console.WriteLine(intsCollection[i]);
+}
+
+List<int> ints = new List<int>();
+for (int i = 0; i < intsCollection.Count; i++)
+{
+    if (intsCollection[i] > 10)
+    ints.Add(intsCollection[i]);
+}
+
+Console.WriteLine("elementy w kolekcji więkrze od 10");
+for (int i = 0; i < ints.Count; i++)
+{
+    Console.WriteLine(ints[i]);
+}
--- Programowanie/ConsoleApp1/Program.cs
+++ Programowanie/ConsoleApp1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿List<int> intsCollection = new List<int>();
+intsCollection.Add(15);
+intsCollection.Add(18);
+intsCollection.Add(5);
+intsCollection.Add(12);
+intsCollection.Add(19);
+intsCollection.Add(4);
+for (int i = 0; i < intsCollection.Count; i++)
+{
+    Console.WriteLine(intsCollection[i]);
+}
