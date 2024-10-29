a3fbafb196bcf21bfeb2526b13cce2570b4756d5
q <q>
2024-10-29 15:53:05 +0100

    Linq

---
+++ Programowanie/LinkConsoleApp/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqConsoleApp
+{
+    struct Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+
+    }
+
+}
---
+++ Programowanie/LinkConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using LinqConsoleApp;
+
+List<Person> people = new List<Person>();
+
+#region uzupełnienie kolekcji
+Person person = new Person();
+person.Name = "Ewa";
+person.Age = 32;
+person.Surname = "Kowalska";
+people.Add(person);
+
+person = new Person();
+person.Name = "Jan";
+person.Age = 12;
+person.Surname = "Nowak";
+people.Add(person);
+
+person = new Person();
+person.Name = "Karol";
+person.Age = 52;
+person.Surname = "Krawczyk";
+people.Add(person);
+
+person = new Person();
+person.Name = "Paweł";
+person.Age = 35;
+person.Surname = "Nowakowski";
+people.Add(person);
+#endregion
+Console.WriteLine("Zawartoś głownej kolekcji: ");
+
+WritePersons(people);
+
+List<Person> sortedPeopleByAge = people.OrderBy(p => p.Age).ToList();
+WritePersons(sortedPeopleByAge);
+
+sortedPeopleByAge = people.OrderByDescending(p => p.Age).ToList();
+WritePersons(sortedPeopleByAge);
+
+Console.WriteLine($"Najstarsza osoba ma: {people.Max(p => p.Age)}");
+Console.WriteLine($"Najmłodsza osoba ma: {people.Min(p => p.Age)}");
+Console.WriteLine($"Średni wiek to: {people.Average(p => p.Age)}");
+
+void WritePersons(List<Person> people)
+{
+    foreach (Person p in people)
+        Console.WriteLine($"Imię: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}");
+}
