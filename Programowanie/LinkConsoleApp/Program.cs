fc99969079cd294ce57faa1a51b849c620060e5a
q <q>
2024-11-18 16:01:28 +0100

    c

--- Programowanie/LinkConsoleApp/Program.cs
+++ Programowanie/LinkConsoleApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿using LinqConsoleApp;
-
+void WritePersons(List<Person> people)
+{
+    foreach (Person p in people)
+        Console.WriteLine($"Imię: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}");
+} 
 List<Person> people = new List<Person>();
 
 #region uzupełnienie kolekcji
@@ -26,6 +30,18 @@ person.Name = "Paweł";
 person.Age = 35;
 person.Surname = "Nowakowski";
 people.Add(person);
+
+person = new Person();
+person.Name = "Paweł";
+person.Age = 5;
+person.Surname = "Nowak";
+people.Add(person);
+
+person = new Person();
+person.Name = "Karol";
+person.Age = 23;
+person.Surname = "Nowak";
+people.Add(person);
 #endregion
 Console.WriteLine("Zawartoś głownej kolekcji: ");
 
@@ -41,8 +57,30 @@ Console.WriteLine($"Najstarsza osoba ma: {people.Max(p => p.Age)}");
 Console.WriteLine($"Najmłodsza osoba ma: {people.Min(p => p.Age)}");
 Console.WriteLine($"Średni wiek to: {people.Average(p => p.Age)}");
 
-void WritePersons(List<Person> people) 
+
+
+Console.Clear();
+
+List<Person> sortedPeople = people.OrderBy(p => p.Surname).ThenBy( p=> p.Age).ToList();
+WritePersons(sortedPeople);
+
+List<Person> mixedPeople = people.OrderBy(p => new Random().Next()).ToList();
+
+Person firstPerson = people.FirstOrDefault(p => p.Name.Length > 3);
+// if(firstPerson != null) // jeżeli Person jest klasą
+if(!firstPerson.Equals(default(Person)))
 {
-    foreach (Person p in people)
-        Console.WriteLine($"Imię: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}");
+    Console.WriteLine($"osoba z imieniem więkrzym niź 3 litery {firstPerson}");
 }
+
+
+int x = 0, y = 2;
+
+
+
+
+
+
+(x, y) = (y, x);
+
+ 
