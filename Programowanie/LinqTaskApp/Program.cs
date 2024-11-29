f0e4e0f3cf485c7e2a93c1e9359db3c2eaa6eeff
q <q>
2024-11-29 12:18:35 +0100

    f

--- Programowanie/LinqTaskApp/Program.cs
+++ Programowanie/LinqTaskApp/Program.cs
@@ -16,8 +16,14 @@ var task4 = tasks.Count(t => t.IsCompleted);
 var task5 = tasks.Select(t => t.Name).ToList();
 
 var task6 = tasks.Where(t => t.IsCompleted).Select(t => t.Name).OrderBy(t => t).ToList();
-//var task7 = tasks.GroupBy(t => t.IsCompleted);
+var task7 = tasks.OrderBy(t => t.IsCompleted).ThenBy(t => t.Name).GroupBy(t => t.IsCompleted);
 var task8 = tasks.Where(t => !t.IsCompleted).OrderBy(t => t.Name.Length).First();
-var task9 = tasks.Where(t => t.IsCompleted).Select(t => t.Name.Length).ToList();
-//var task10 = tasks.Where(t => t.IsCompleted).Select(t => t.Name);
-
+var task9 = tasks.Where(t => t.IsCompleted).Select(t => t.Name.Length).Sum();
+var task10 = tasks.Where(t => t.IsCompleted).Select((t, index) => new { Index = index + 1, T = t });
+var task11 = tasks.GroupBy(t => t.IsCompleted)
+    .Select(g => new { GroupVaule = g.Key, MaxName = g.OrderByDescending(t => t.Name.Length).First().Name });
+var task12 = tasks.GroupBy(t => t.IsCompleted).Select(g=> new {GroupName = g.Key, Count = g.Where(p => p.Name.Contains("the")).Count()});
+var task13 = tasks.Where(t => t.IsCompleted).Select((g,Index) => new { length = g.Name.Length, index = Index+1 });
+var task14 = tasks.OrderBy(t => t.IsCompleted).ThenBy(t => t.Name);
+var task15 = tasks.All(t => t.Name.ToLower().Where(c => "aeiouy".Contains(c)).Distinct().Count() >= 2);
+var task16 = tasks.Where(t => t.IsCompleted).SelectMany(t => t.Name.Where(c=> char.IsLetter(c)).Distinct());
