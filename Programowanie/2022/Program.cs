1dde7acb56d1c082523b849106332d51aa95a18b
q <q>
2025-02-07 11:31:42 +0100

    bruh

--- Programowanie/2022/Program.cs
+++ Programowanie/2022/Program.cs
@@ -10,7 +10,33 @@ sr.Close();
 
 //4.1
 
-int counter4_1 = numbers.Where(s => s[0] == s[s.Length-1]).Count();
-string firstNumber4_1 = numbers.First(s => s[0] == s[s.Length - 1]);
+//int counter4_1 = numbers.Where(s => s[0] == s[s.Length-1]).Count();
+//string firstNumber4_1 = numbers.First(s => s[0] == s[s.Length - 1]);
 
-Console.WriteLine($"{counter4_1} {firstNumber4_1}");
+//Console.WriteLine($"{counter4_1} {firstNumber4_1}");
+
+//4.2
+int MaxPrimeNumbers = 0;
+for (int i = 0; i < numbers.Count; i++)
+{
+    List<int> PrimeNumbers = new List<int>();
+
+    int numb = int.Parse(numbers[0]);
+    for (int j = 2; j <= numb; j++)
+    {
+        if (j == int.Parse(numbers[i]))
+        {
+            break;
+        }
+        while (numb % j == 0)
+        {
+            PrimeNumbers.Add(j);
+            numb /= j;
+        }
+    }
+    if(PrimeNumbers.Count > MaxPrimeNumbers)
+    {
+        MaxPrimeNumbers = PrimeNumbers.Count;
+    }
+}
+Console.WriteLine(MaxPrimeNumbers);
---
+++ Programowanie/Matura2/Program.cs
@@ -0,0 +1,38 @@
+﻿StreamReader streamReader = new StreamReader("przyklad.txt");
+string num;
+streamReader.ReadLine();
+List<string> numbers = new List<string>();
+while ((num = streamReader.ReadLine()) != null)
+{
+    numbers.Add(num);
+}
+streamReader.Close();
+
+foreach (string number in numbers)
+{
+
+    string n = new string(number.Reverse().ToArray());
+    if (int.Parse(n) % 17 == 0)
+    {
+        //Console.WriteLine(n);
+    }
+}
+
+//4.2
+int highest = 0;
+
+foreach (string number in numbers)
+{
+    int n = Math.Abs((int.Parse(number)));
+
+    string sNumberReverse = new string(number.Reverse().ToArray());
+    int numberReverse = Math.Abs(int.Parse(sNumberReverse));
+
+    if (highest < n - numberReverse)
+    {
+        highest = n;
+    }
+}
+Console.WriteLine(highest);
+
+//4.3   
