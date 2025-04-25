91a4fd2d756f4395d98f37ddb40e7bc78f804bfc
Paweł Łata <116968696+srupakij731@users.noreply.github.com>
2025-04-25 07:10:12 +0200

    Matura 2

--- Programowanie/Matura2/Program.cs
+++ Programowanie/Matura2/Program.cs
@@ -8,31 +8,80 @@ while ((num = streamReader.ReadLine()) != null)
 }
 streamReader.Close();
 
+Console.WriteLine("4.1\n");
 foreach (string number in numbers)
 {
 
-    string n = new string(number.Reverse().ToArray());
+    string n = reverse(number);
     if (int.Parse(n) % 17 == 0)
     {
-        //Console.WriteLine(n);
+        Console.WriteLine(n);
     }
 }
 
-//4.2
+Console.WriteLine("\n4.2");
 int highest = 0;
 
 foreach (string number in numbers)
 {
     int n = Math.Abs((int.Parse(number)));
 
-    string sNumberReverse = new string(number.Reverse().ToArray());
+    string sNumberReverse = reverse(number);
     int numberReverse = Math.Abs(int.Parse(sNumberReverse));
 
-    if (highest < n - numberReverse)
+    if (highest < (n - numberReverse))
     {
-        highest = n;
+        highest = number;
     }
 }
 Console.WriteLine(highest);
+Console.WriteLine(Math.Abs(int.Parse(highest) - int.Parse(reverse(highest))));
 
-//4.3   
+Console.WriteLine("\n4.3");
+foreach(string number in numbers)
+{
+    if(isPrimaryNumber(number) && isPrimaryNumber(reverse(number)))
+        Console.WriteLine(number);
+
+}
+
+Console.WriteLine("\n4.4");
+int uniqueNumbers = 0;
+int twoNumbers = 0;
+int threeNumbers = 0;
+var numbersGrouped = numbers.GroupBy(n => n);
+
+foreach(var group in numbersGrouped)
+{
+    if(group.Count() == 1)
+        uniqueNumbers++;
+    if(group.Count() == 2)
+        twoNumbers++;
+    if(group.Count() == 3)
+        threeNumbers++;
+}
+Console.WriteLine(uniqueNumbers);
+Console.WriteLine(twoNumbers);
+Console.WriteLine(threeNumbers);
+
+
+string reverse(string number)
+{
+    string n ="";
+    for(int i = number.length-1; i >= 0; i--)
+    {
+        n += number[i];
+    }
+    return n;
+}
+
+bool isPrimaryNumber(string number)
+{
+    int n = int.Parse(number);
+    for(int i = 2; i < n; i++)
+    {
+        if(n % i == 0)
+            return false;
+    }
+    return true;
+}
